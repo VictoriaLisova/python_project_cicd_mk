8d73b20164100ce098650d8c970d4537cb198d56
Victoria Lisova <lisovaviktoria268@gmail.com>
2024-04-05 09:30:41 +0300

    get count of words

--- data_analysis.py
+++ data_analysis.py
@@ -13,6 +13,15 @@ def get_count_of_sentences(sentences: list) -> int:
     return len(sentences)
 
 
+def get_count_of_words(sentences: list) -> int:
+    words_count = 0
+    for sentence in sentences:
+        words = re.split(r', |\s|,|;|:', sentence)
+        words_count += len(words)
+    return words_count
+
+
 sentences_list = get_sentences("text.txt")
 print(get_count_of_sentences(sentences_list))
+print(get_count_of_words(sentences_list))
 
