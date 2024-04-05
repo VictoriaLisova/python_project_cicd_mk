4cf24698286a6bf0f93b528b2d8d2b4e81b275ab
Victoria Lisova <lisovaviktoria268@gmail.com>
2024-04-05 09:40:30 +0300

    write data to file

--- data_analysis.py
+++ data_analysis.py
@@ -2,6 +2,11 @@ import re
 
 
 def get_sentences(file_name: str) -> list:
+    """
+    get all sentences from text file that end with ., ?, ! or ...
+    :param file_name: file with text
+    :return: list of sentences
+    """
     result = []
     with open(file_name) as file:
         for line in file:
@@ -10,10 +15,20 @@ def get_sentences(file_name: str) -> list:
 
 
 def get_count_of_sentences(sentences: list) -> int:
+    """
+    get the length of list of sentences
+    :param sentences: list of sentences
+    :return: count of sentences
+    """
     return len(sentences)
 
 
 def get_count_of_words(sentences: list) -> int:
+    """
+    go throw each sentence and split into words based on signs: ",", "space", ", ", ":", ";"
+    :param sentences: list of sentences
+    :return: count of words
+    """
     words_count = 0
     for sentence in sentences:
         words = re.split(r', |\s|,|;|:', sentence)
@@ -21,7 +36,24 @@ def get_count_of_words(sentences: list) -> int:
     return words_count
 
 
-sentences_list = get_sentences("text.txt")
-print(get_count_of_sentences(sentences_list))
-print(get_count_of_words(sentences_list))
+def write_to_file_result(words_count: str, sentences_count: str, file_name: str) -> str:
+    """
+    write results about count of sentences and words into file
+    :param words_count: count of words
+    :param sentences_count: count of sentences
+    :param file_name: file to write result
+    :return: file with results
+    """
+    with open(file_name, "w") as file:
+        lines = [f"Count of sentences: {sentences_count}\n",
+                 f"Count of words: {words_count}"]
+        file.writelines(lines)
+    return file_name
 
+
+sentences_list = get_sentences("text.txt")
+sentences_count = get_count_of_sentences(sentences_list)
+print(sentences_count)
+words_count = get_count_of_words(sentences_list)
+print(words_count)
+write_to_file_result(words_count, sentences_count, "result.txt")
