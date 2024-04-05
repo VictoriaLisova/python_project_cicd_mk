4aa1aa9667547b60a4a8c13117ed9bf39cceb553
Victoria Lisova <lisovaviktoria268@gmail.com>
2024-04-05 09:30:14 +0300

    get count of sentences

--- data_analysis.py
+++ data_analysis.py
@@ -12,6 +12,7 @@ def get_sentences(file_name: str) -> list:
 def get_count_of_sentences(sentences: list) -> int:
     return len(sentences)
 
+
 sentences_list = get_sentences("text.txt")
 print(get_count_of_sentences(sentences_list))
 
